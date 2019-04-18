2c93a3ec422dda294b55352ff4e15d08a1fe8287
Omri Lotan <omrilotan@users.noreply.github.com>
2019-04-18 13:46:58 +0100

    fixtures

--- README.md
+++ README.md
@@ -6,6 +6,30 @@
 const parse = require('rds-slow-log-parse');
 
 const payload = new Buffer(event.awslogs.data, 'base64');
-const logs = JSON.parse(zlib.gunzipSync(payload).toString(‘ascii’));
-const events = logs.map(parse);
+const logs = JSON.parse(zlib.gunzipSync(payload).toString('ascii'));
+
+const events = logs.map(parse); // <= ✨ Money time
+```
+
+### Example
+
+Record
+```
+# Time: 190227 9:46:00 # User@Host: rdsadmin[rdsadmin] @ localhost [127.0.0.1] Id: 9059049 # Query_time: 0.000202 Lock_time: 0.000077 Rows_sent: 1 Rows_examined: 1 SET timestamp=1551260760; SELECT count(*) from mysql.rds_table_status WHERE action = 'reset slave' and master_host is NULL and master_port is NULL ORDER BY action_timestamp LIMIT 1;
+```
+
+Result
+```js
+{
+    user: 'rdsadmin',
+    host: 'localhost',
+    ip: '127.0.0.1',
+    id: '9059049',
+    duration: 0.000202,
+    lock_wait: 0.000077,
+    rows_sent: 1,
+    rows_examined: 1,
+    timestamp: 1551260760000,
+    query: `SELECT count(*) from mysql.rds_table_status WHERE action = 'reset slave' and master_host is NULL and master_port is NULL ORDER BY action_timestamp LIMIT 1`,
+}
 ```
--- fixtures/index.js
+++ fixtures/index.js
@@ -30,7 +30,7 @@ module.exports = [
         },
     ],
     [
-        `2019-04-18T08:55:36.001Z   465aa33d-83d4-4ac3-8198-d94e19993b25    # Time: 190418 8:55:34 # User@Host: root[root] @ [172.30.0.229] Id: 19348 # Query_time: 3.000551 Lock_time: 0.000000 Rows_sent: 1 Rows_examined: 0 SET timestamp=1555577734; select sleep(3);`,
+        `# Time: 190418 8:55:34 # User@Host: root[root] @ [172.30.0.229] Id: 19348 # Query_time: 3.000551 Lock_time: 0.000000 Rows_sent: 1 Rows_examined: 0 SET timestamp=1555577734; select sleep(3);`,
         {
             user: 'root',
             host: '',
@@ -45,12 +45,9 @@ module.exports = [
         },
     ],
     [
-        `2019-04-18T08:55:36.001Z
-465aa33d-83d4-4ac3-8198-d94e19993b25
-# Time: 190418 8:55:34
+        `# Time: 190418 8:55:34
 # User@Host: root[root] @ [172.30.0.229] Id: 19348
-# Query_time: 3.000551 Lock_time: 0.000000 Rows_sent: 1 Rows_examined: 0 SET timestamp=1555577734;
-select sleep(3);`,
+# Query_time: 3.000551 Lock_time: 0.000000 Rows_sent: 1 Rows_examined: 0 SET timestamp=1555577734; select sleep(3);`,
         {
             user: 'root',
             host: '',
@@ -62,6 +59,21 @@ select sleep(3);`,
             rows_examined: 0,
             timestamp: 1555577734000,
             query: `select sleep(3)`,
+        },
+    ],
+    [
+        `2019-04-18T08:55:36.001Z   465aa33d-83d4-4ac3-8198-d94e19993b25   # Time: 190418 12:29:49\n# User@Host: root[root] @ [172.30.0.229] Id: 19348\n# Query_time: 1.099867 Lock_time: 0.000000 Rows_sent: 1 Rows_examined: 0\nSET timestamp=1555590589;\nselect sleep(1.1);`,
+        {
+            user: 'root',
+            host: '',
+            ip: '172.30.0.229',
+            id: '19348',
+            duration: 1.099867,
+            lock_wait: 0,
+            rows_sent: 1,
+            rows_examined: 0,
+            timestamp: 1555590589000,
+            query: `select sleep(1.1)`,
         },
     ],
 ];
